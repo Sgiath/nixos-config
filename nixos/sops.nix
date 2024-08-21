62347c7ca4c0613f7845c848ba1068c5e4da8e6f
sgiath <sgiath@sgiath.dev>
2024-08-21 07:05:21 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- nixos/sops.nix
+++ nixos/sops.nix
@@ -1,7 +1,3 @@
 {
-  imports = [ <sops-nix/modules/sops> ];
-  # This will add secrets.yml to the nix store
-  # You can avoid this by adding a string to the full path instead, i.e.
-  # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
   sops = {};
 }
