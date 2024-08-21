205b24c84462aa58f0ee7697a970a4084cf69aea
sgiath <sgiath@sgiath.dev>
2024-08-21 09:15:00 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- work/aws.nix
+++ work/aws.nix
@@ -32,7 +32,7 @@ in
     ];
 
     file = {
-      ".docker/config.json".text = pkgs.formats.yaml.toString {
+      ".docker/config.json".text = builtins.toJSON {
         credHelpers = {
           "public.ecr.aws" = "ecr-login";
           "173509387151.dkr.ecr.us-east-1.amazonaws.com" = "ecr-login";
