1946764976be27b2af9cce68d349989c7257c590
sgiath <sgiath@sgiath.dev>
2024-09-26 21:04:28 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- pkgs/stt/stt.py
+++ pkgs/stt/stt.py
@@ -1,14 +1,12 @@
 import sys
 from faster_whisper import WhisperModel
 
-model_size = "large-v3"
-
 # Run on GPU with FP16
 # model = WhisperModel(model_size, device="cuda", compute_type="float16")
 # or run on GPU with INT8
 # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
-model = WhisperModel(model_size, device="cpu", compute_type="int8")
+model = WhisperModel("tiny", device="cpu", compute_type="int8")
 
 segments, info = model.transcribe(sys.argv[1], beam_size=5)
 
