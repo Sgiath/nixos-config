5d7dc9cbd24d1a679483b18e4aa8dc0ed4764c0a
sgiath <sgiath@sgiath.dev>
2024-10-06 19:54:06 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/ssh.nix
+++ modules/home/sgiath/ssh.nix
@@ -5,21 +5,21 @@
       compression = true;
       serverAliveInterval = 60;
       serverAliveCountMax = 30;
-      extraConfig = ''
-        User sgiath
-        Port 22
-        Protocol 2
-
-        PasswordAuthentication yes
-        ChallengeResponseAuthentication yes
-        PubkeyAuthentication yes
-        PreferredAuthentications publickey
-
-        Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr
-        KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
-        HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
-        MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256
-      '';
+      # extraConfig = ''
+      #   User sgiath
+      #   Port 22
+      #   Protocol 2
+      #
+      #   PasswordAuthentication yes
+      #   ChallengeResponseAuthentication yes
+      #   PubkeyAuthentication yes
+      #   PreferredAuthentications publickey
+      #
+      #   Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr
+      #   KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+      #   HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
+      #   MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256
+      # '';
 
       matchBlocks = {
         # git
