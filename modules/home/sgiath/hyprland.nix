5f912c9307958e470d1cf9d179f6323ab183a300
sgiath <sgiath@sgiath.dev>
2024-11-07 07:16:30 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland.nix
+++ modules/home/sgiath/hyprland.nix
@@ -10,6 +10,7 @@
   };
 
   config = lib.mkIf config.programs.hyprland.enable {
+    stylix.targets.hyprland.enable = false;
     wayland.windowManager.hyprland = {
       enable = true;
       package = pkgs.hyprland;
