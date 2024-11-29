ad912725e1cc360f18a9e9d112597dd98cc2f54c
sgiath <sgiath@sgiath.dev>
2024-11-29 14:22:45 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs-master.url = "nixpkgs/master";
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.11";
-    nixpkgs-proton.url = "github:daniel-fahey/nixpkgs/protonmail-bridge-gui-qt68support";
+    nixpkgs-proton.url = "github:r-ryantm/nixpkgs/auto-update/protonmail-bridge";
 
     home-manager = {
       url = "github:nix-community/home-manager";
