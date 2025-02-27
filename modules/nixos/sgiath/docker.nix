1582b331afdccb67cc3322e8807b97875fd0eecd
sgiath <sgiath@sgiath.dev>
2025-02-27 11:25:07 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/docker.nix
+++ modules/nixos/sgiath/docker.nix
@@ -9,10 +9,8 @@
   };
 
   config = lib.mkIf config.sgiath.docker.enable {
-    virtualisation.docker.enable = false;
-    virtualisation.podman = {
-      enable = true;
-    };
-    # users.users.sgiath.extraGroups = [ "docker" ];
+    virtualisation.docker.enable = true;
+    virtualisation.podman.enable = true;
+    users.users.sgiath.extraGroups = [ "docker" ];
   };
 }
