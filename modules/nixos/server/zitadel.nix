868e78a49b20f7ddec2a1308d7a6e8f11b517748
sgiath <sgiath@sgiath.dev>
2025-04-02 11:01:31 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

---
+++ modules/nixos/server/zitadel.nix
@@ -0,0 +1,31 @@
+{ lib, config, ... }:
+{
+  config = lib.mkIf (config.sgiath.server.enable && config.services.zitadel.enable) {
+    services = {
+      zitadel = {
+        settings = {
+          Port = 8087;
+        };
+        tlsMode = "external";
+      };
+
+      nginx.virtualHosts."auth.sgiath.dev" = {
+        # SSL
+        onlySSL = true;
+        kTLS = true;
+
+        # ACME
+        enableACME = true;
+        acmeRoot = null;
+
+        # QUIC
+        http3_hq = true;
+        quic = true;
+
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8087";
+        };
+      };
+    };
+  };
+}
