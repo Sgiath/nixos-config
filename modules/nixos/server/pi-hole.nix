a12f5e0efa0045ad0bc0bd8ba6a8cb4f9d1433f5
sgiath <sgiath@sgiath.dev>
2025-04-16 19:29:07 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/pi-hole.nix
+++ modules/nixos/server/pi-hole.nix
@@ -23,7 +23,7 @@
     };
 
     virtualisation.oci-containers.containers.pihole = {
-      image = "pihole/pihole:2024.07.0";
+      image = "pihole/pihole:2025.04.0";
       ports = [
         "53:53/tcp"
         "53:53/udp"
