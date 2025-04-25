f5f52ada24e4e2981c4bdcbce1a183c60ae420aa
sgiath <sgiath@sgiath.dev>
2025-04-25 07:06:21 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/networking.nix
+++ modules/nixos/sgiath/networking.nix
@@ -49,6 +49,7 @@ in
           "focalboard.sgiath.dev"
           "auth.sgiath.dev"
           "osm.sgiath.dev"
+          "photo.sgiath.dev"
         ];
         "192.168.1.4" = [ "nas.sgiath" ];
         "192.168.1.5" = [ "nas.sgiath" ];
