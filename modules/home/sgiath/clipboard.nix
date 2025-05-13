58bf9403ace1019d5dc30fc112002b84ee0d70a8
sgiath <sgiath@sgiath.dev>
2025-05-13 08:51:42 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/clipboard.nix
+++ modules/home/sgiath/clipboard.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 {
   services.clipse.enable = true;
+  home.packages = with pkgs; [
+    wl-clipboard
+    wl-clipboard-x11
+  ];
   wayland.windowManager.hyprland.settings = {
     bind = [ "$mod, V, exec, ${pkgs.kitty}/bin/kitty --class clipse -e ${pkgs.clipse}/bin/clipse" ];
     windowrulev2 = [
