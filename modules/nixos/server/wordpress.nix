216db2f66ee8a3ddc3428a5716652e45f45c74a1
sgiath <sgiath@sgiath.dev>
2025-07-27 08:38:17 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/server/wordpress.nix
+++ modules/nixos/server/wordpress.nix
@@ -1,52 +1,13 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 {
   options.services.wordpress.proxy = lib.mkEnableOption "Wordpress proxy";
 
   config = lib.mkIf (config.sgiath.server.enable && config.services.wordpress.proxy) {
-    virtualisation.oci-containers.containers.romana = {
-      image = "romana-vaverova:latest";
-      imageFile = pkgs.dockerTools.buildImage {
-        name = "romana-vaverova";
-        tag = "latest";
-
-        fromImageName = "wordpress";
-        fromImageTag = "6.8.2-php8.4-apache";
-
-        runAsRoot = ''
-          apt-get update && apt-get install -y libxml2-dev
-          docker-php-ext-install soap
-        '';
-      };
-
-      ports = [ "80:8081" ];
-      volumes = [ "/data/www/romana-vaverova.cz:/var/www/html" ];
-      extraOptions = [ "--network=host" ];
-      environment = {
-        WORDPRESS_DB_USER = "wordpress";
-        WORDPRESS_DB_NAME = "romana-vaverova";
-        WORDPRESS_DB_HOST = "localhost";
-        # WORDPRESS_DB_PASSWORD = "wordpress";
-      };
-    };
-
     services = {
-      mysql = {
-        enable = true;
-        package = pkgs.mariadb;
-        ensureDatabases = [ "romana-vaverova" ];
-        ensureUsers = [
-          {
-            name = "wordpress";
-            ensurePermissions = { "romana-vaverova.*" = "ALL PRIVILEGES"; };
-          }
-        ];
-      };
-
       nginx.virtualHosts."romana.sgiath.dev" = {
         # SSL
         onlySSL = true;
