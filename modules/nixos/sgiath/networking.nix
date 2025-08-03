c95b2b1f616a7409794403a8a7167429ffa635d4
Sgiath <sgiath@sgiath.dev>
2025-08-03 14:25:48 +0000

    changes
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/networking.nix
+++ modules/nixos/sgiath/networking.nix
@@ -12,6 +12,7 @@ in
         networks = {
           "Turris 5".pskRaw = "5979f534aeb44615c6efa716ad68f0bf36747981e4eb1a83381997c03301eb44";
           "Turris 2".pskRaw = "70130aabbf56025a53266182e3a282588d7c324c90f4e75e45fff5a275ba019a";
+          "mlyn".pskRaw = "e4b2f70b2546be145714c58c42b6f0d725b4df7b5d06dbc77bd439157ca7147f";
         };
       };
 
