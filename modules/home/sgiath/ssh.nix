5688ea4ee64602ec40e6d242e0efac474e1692db
sgiath <sgiath@sgiath.dev>
2025-08-27 07:01:55 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/ssh.nix
+++ modules/home/sgiath/ssh.nix
@@ -2,23 +2,7 @@
 {
   config = lib.mkIf config.programs.ssh.enable {
     programs.ssh = {
-      compression = true;
       enableDefaultConfig = false;
-      extraConfig = ''
-        User sgiath
-        Port 22
-        Protocol 2
-
-        PasswordAuthentication yes
-        ChallengeResponseAuthentication yes
-        PubkeyAuthentication yes
-        PreferredAuthentications publickey
-
-        Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr
-        KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
-        HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
-        MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256
-      '';
 
       matchBlocks = {
         # git
@@ -57,6 +41,29 @@
         "i.*.crazyegg.com" = {
           user = "crazyegg";
           proxyCommand = "ssh bastion.crazyegg.com -W %h:%p";
+        };
+
+        # defaults
+        "*" = {
+          user = "sgiath";
+          port = 22;
+          compression = true;
+          serverAliveInterval = 60;
+          serverAliveCountMax = 30;
+
+          extraConfig = ''
+            Protocol 2
+    
+            PasswordAuthentication yes
+            ChallengeResponseAuthentication yes
+            PubkeyAuthentication yes
+            PreferredAuthentications publickey
+    
+            Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes256-ctr
+            KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256
+            HostKeyAlgorithms ssh-ed25519-cert-v01@openssh.com,ssh-rsa-cert-v01@openssh.com,ssh-ed25519,ssh-rsa
+            MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512,hmac-sha2-256
+          '';
         };
       };
     };
