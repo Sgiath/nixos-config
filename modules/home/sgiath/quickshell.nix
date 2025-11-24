76345ae0d71b1d08cf7b0152f17b5d15fa46c423
sgiath <sgiath@sgiath.dev>
2025-11-24 08:20:06 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/quickshell.nix
+++ modules/home/sgiath/quickshell.nix
@@ -1,6 +1,29 @@
 { inputs, pkgs, ... }:
 let
-  pythonEnv = pkgs.pythonEnv;
+  pythonEnv = pkgs.python3.withPackages (ps: [
+    ps.build
+    ps.cffi
+    ps.click
+    ps."dbus-python"
+    ps."kde-material-you-colors"
+    ps.libsass
+    ps.loguru
+    ps."material-color-utilities"
+    ps.materialyoucolor
+    ps.numpy
+    ps.pillow
+    ps.psutil
+    ps.pycairo
+    ps.pygobject3
+    ps.pywayland
+    ps.setproctitle
+    ps."setuptools-scm"
+    ps.tqdm
+    ps.wheel
+    ps."pyproject-hooks"
+    ps.opencv4
+  ]);
+  
   quickshell-with-qtpositioning = pkgs.symlinkJoin {
       name = "quickshell-with-qtpositioning";
       paths = [ inputs.quickshell.packages.${pkgs.system}.default ];
