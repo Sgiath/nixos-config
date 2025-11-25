2c964711663c74df33d24fa4bd631c5d51050587
sgiath <sgiath@sgiath.dev>
2025-11-25 10:39:36 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland/layout.nix
+++ modules/home/sgiath/hyprland/layout.nix
@@ -1,7 +1,15 @@
 {
   wayland.windowManager.hyprland.settings = {
-    general.layout = "master";
+    general.layout = "dwindle";
 
+    dwindle = {
+      force_split = 2;
+      preserve_split = true;
+      smart_split = true;
+      split_width_multiplier = 2.0;
+      default_split_ratio = 0.5;
+    };
+    
     master = {
       mfact = 0.75;
       new_status = "master";
