4b730333d13a0312716f3a5bf4154ba134553b25
sgiath <sgiath@sgiath.dev>
2025-11-25 09:16:14 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland/layout.nix
+++ modules/home/sgiath/hyprland/layout.nix
@@ -1,9 +1,15 @@
 {
   wayland.windowManager.hyprland.settings = {
     general.layout = "master";
+
     master = {
-      orientation = "right";
+      orientation = "center";
       new_status = "master";
+      mfact = 0.70;
+      default_split_ratio = 0.5;
+      force_split = 2;
+      slave_count_for_center_master = 0;
+      center_master_fallback = "right";
     };
   };
 }
