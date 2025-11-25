b79290208af5c8db295c1ee7ef4ea85a1617edd4
sgiath <sgiath@sgiath.dev>
2025-11-25 09:18:09 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/hyprland/layout.nix
+++ modules/home/sgiath/hyprland/layout.nix
@@ -3,11 +3,9 @@
     general.layout = "master";
 
     master = {
-      orientation = "center";
-      new_status = "master";
       mfact = 0.70;
-      default_split_ratio = 0.5;
-      force_split = 2;
+      new_status = "master";
+      orientation = "center";
       slave_count_for_center_master = 0;
       center_master_fallback = "right";
     };
