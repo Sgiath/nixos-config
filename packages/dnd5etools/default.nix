c8c2aa3de7cead6f36b2acbb3672d874bab8df12
sgiath <sgiath@sgiath.dev>
2026-01-01 23:59:40 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- packages/dnd5etools/default.nix
+++ packages/dnd5etools/default.nix
@@ -9,57 +9,57 @@
   ...
 }:
 let
-  version = "2.19.1";
+  version = "2.22.0";
   pname = "5etools";
 
   imgHashes = [
     {
       name = "z01";
-      hash = "sha256-Aly1WvxmBipgSSD8YjnvxvvVhFCxpP3R75uG3yWPpIc=";
+      hash = "sha256-fWir9jw0de03Nr7IPgBV74Q00AFibf50zuFLCveGuxo=";
     }
     {
       name = "z02";
-      hash = "sha256-DaQpjcjhMQhYX/MRP5lKx1z788V4USDpmi1TLpR+xtg=";
+      hash = "sha256-B05o/amcw2T/a0XOhiQSia2iK5oPPQRBY7lj+en/eio=";
     }
     {
       name = "z03";
-      hash = "sha256-Tn/yeIpnpRoT+J9l2VWCA3ejj9zBke6JPHQasMPUmMQ=";
+      hash = "sha256-DCEFaGlY7VgHlz9ax6BBDg1ob7Jedg63BbtH0fjsf6Q=";
     }
     {
       name = "z04";
-      hash = "sha256-Kr96Tu4qOGHCHYecsOFLW9u71bBkgwSqn7NL+0fLYSw=";
+      hash = "sha256-x2yQ352Ybv5Ja2eyzTKUYXGvNB0mrNQH8Gfurr+33SM=";
     }
     {
       name = "z05";
-      hash = "sha256-OvUGu6OQ4WB/c7Sny1PliA2o/w4bESXFWQNJz/eQVgU=";
+      hash = "sha256-+tmf1NQDFRXv35q/t0rXyEynaORs/VvlD3lmoUQgqg8=";
     }
     {
       name = "z06";
-      hash = "sha256-6G4uWpp32ndqSqpDhn6BYMcS5fj+3Ap6QwJAZa69NUM=";
+      hash = "sha256-d2aAgNMaApXkmJBQAJL4vb5izQmRAnA+LcZoQrimwqE=";
     }
     {
       name = "z07";
-      hash = "sha256-2pHORnuaRJohqF2w/S5St7EAQSHZqG9EZzPqbUNtx5E=";
+      hash = "sha256-gfJAwQ5pfSF449Cwt7UhpnHbS/7dsJTvqgsvebq2Gmg=";
     }
     {
       name = "z08";
-      hash = "sha256-Y3d8APR82mck3xIPc2Ak3IJCnxji2TtBvo66dGLM+Jw=";
+      hash = "sha256-3YSFupQq3M+BEPGL5PnmOXECVjPYQzox9FhfJv7C0wc=";
     }
     {
       name = "z09";
-      hash = "sha256-l8TPnxEp4ZQQdKRY7c1+Lxg2Leot/ybd2NV8AWPz2Uk=";
+      hash = "sha256-jtYMF8Kf/sjsMn4foeI0Xp9mAJeXbH3K50QEU9LKhyE=";
     }
     {
       name = "z10";
-      hash = "sha256-uWiTrD25xwHylKd/lcv29GJjtC33CxiH/W0bJ9eT7J0=";
+      hash = "sha256-juuMKUKeBv1vH3fbS3mjLJ2TL/vyQRi35m7xeAkE6s4=";
     }
     {
       name = "z11";
-      hash = "sha256-/5sgPsiIomRGVYyMa/OExscDnB8eU4j3SkBgWppa+1A=";
+      hash = "sha256-o5RUCmgLj+qR/Ypa3GhsrCTiZXgy9CsIOtE8noKODQE=";
     }
     {
       name = "zip";
-      hash = "sha256-N5rURyG3iqE0SD3qUJB5j/NXzD1mWVE9KR08OWpOJnw=";
+      hash = "sha256-hAi9uhyW8h7CA5E5Hh1rQ5DEGgc835W/8Ie7P0HTaYo=";
     }
   ];
 
@@ -68,7 +68,7 @@ let
     let
       img = fetchurl {
         pname = "5etools-img-${v.name}";
-        version = "2.19.1";
+        version = "2.22.0";
         # inherit version;
         inherit (v) hash;
         url = "https://github.com/5etools-mirror-2/5etools-img/releases/download/v${version}/img-v${version}.${v.name}";
@@ -89,7 +89,7 @@ stdenv.mkDerivation {
     pname = "5etools-src";
     url = "https://github.com/5etools-mirror-3/5etools-src/releases/download/v${version}/${pname}-v${version}.zip";
     stripRoot = false;
-    hash = "sha256-tECi3R/6WiUM4CwzDjfPKieljqkLJRx5tNgmIXKnc7c=";
+    hash = "sha256-HgmyXWeq9JfkRMaU8pl6EXQySAAKHEya5G0sEhh9bq0=";
   };
 
   buildInputs = [
---
+++ packages/dnd5etools/imgHashes.nix
@@ -0,0 +1,50 @@
+imgHashes = [
+  {
+    name = "z01";
+    hash = "sha256-fWir9jw0de03Nr7IPgBV74Q00AFibf50zuFLCveGuxo=";
+  }
+  {
+    name = "z02";
+    hash = "sha256-B05o/amcw2T/a0XOhiQSia2iK5oPPQRBY7lj+en/eio=";
+  }
+  {
+    name = "z03";
+    hash = "sha256-DCEFaGlY7VgHlz9ax6BBDg1ob7Jedg63BbtH0fjsf6Q=";
+  }
+  {
+    name = "z04";
+    hash = "sha256-x2yQ352Ybv5Ja2eyzTKUYXGvNB0mrNQH8Gfurr+33SM=";
+  }
+  {
+    name = "z05";
+    hash = "sha256-+tmf1NQDFRXv35q/t0rXyEynaORs/VvlD3lmoUQgqg8=";
+  }
+  {
+    name = "z06";
+    hash = "sha256-d2aAgNMaApXkmJBQAJL4vb5izQmRAnA+LcZoQrimwqE=";
+  }
+  {
+    name = "z07";
+    hash = "sha256-gfJAwQ5pfSF449Cwt7UhpnHbS/7dsJTvqgsvebq2Gmg=";
+  }
+  {
+    name = "z08";
+    hash = "sha256-3YSFupQq3M+BEPGL5PnmOXECVjPYQzox9FhfJv7C0wc=";
+  }
+  {
+    name = "z09";
+    hash = "sha256-jtYMF8Kf/sjsMn4foeI0Xp9mAJeXbH3K50QEU9LKhyE=";
+  }
+  {
+    name = "z10";
+    hash = "sha256-juuMKUKeBv1vH3fbS3mjLJ2TL/vyQRi35m7xeAkE6s4=";
+  }
+  {
+    name = "z11";
+    hash = "sha256-o5RUCmgLj+qR/Ypa3GhsrCTiZXgy9CsIOtE8noKODQE=";
+  }
+  {
+    name = "zip";
+    hash = "sha256-hAi9uhyW8h7CA5E5Hh1rQ5DEGgc835W/8Ie7P0HTaYo=";
+  }
+];
