861cbb324dce9b9a045d73e7ae2cc7d9561b1148
sgiath <sgiath@sgiath.dev>
2026-01-06 05:53:21 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/default.nix
+++ modules/nixos/sgiath/default.nix
@@ -69,12 +69,14 @@ in
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "claude-code.cachix.org-1:YeXf2aNu7UTX8Vwrze0za1WEDS+4DuI2kVeWEE4fsRk="
         ];
         substituters = [
           "https://cache.nixos.org"
           "https://nix-community.cachix.org"
           "https://nixpkgs-wayland.cachix.org"
           "https://hyprland.cachix.org"
+          "https://claude-code.cachix.org"
         ];
       };
       channel.enable = false;
