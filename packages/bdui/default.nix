7f62c7550559561e8ff11c0396422af153f8086a
sgiath <sgiath@sgiath.dev>
2026-01-13 10:02:31 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- packages/bdui/default.nix
+++ packages/bdui/default.nix
@@ -1,27 +1,75 @@
 {
   lib,
   stdenv,
-  fetchurl,
+  fetchFromGitHub,
+  bun,
   autoPatchelfHook,
+  cacert,
 }:
+let
+  src = fetchFromGitHub {
+    owner = "assimelha";
+    repo = "bdui";
+    rev = "v0.2.0";
+    hash = "sha256-PbJT5MDCjvslXL4MJAZXW2lq3N1T2xyWFhty9AT3YcA=";
+  };
 
+  # Fixed-output derivation for node_modules
+  node_modules = stdenv.mkDerivation {
+    name = "bdui-node-modules";
+    inherit src;
+
+    nativeBuildInputs = [
+      bun
+      cacert
+    ];
+
+    impureEnvVars = lib.fetchers.proxyImpureEnvVars;
+    SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+
+    buildPhase = ''
+      export HOME=$TMPDIR
+      bun install --frozen-lockfile
+    '';
+
+    installPhase = ''
+      mkdir -p $out
+      cp -r node_modules $out/
+    '';
+
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
+    outputHash = "sha256-xtsWbeCM+4td6DJ0FWfHstR8Vlag1hUJEi3iRajI3/I=";
+  };
+in
 stdenv.mkDerivation rec {
   pname = "bdui";
   version = "0.2.0";
 
-  src = fetchurl {
-    url = "https://github.com/assimelha/bdui/releases/download/v${version}/bdui-linux-x64";
-    hash = "sha256-f+O7vrd5xRIwQchjJ+Z2bJo/H7g+ig9dmEtDFqEdzLY=";
-  };
+  inherit src;
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = [
+    bun
+    autoPatchelfHook
+  ];
 
-  dontUnpack = true;
-  dontBuild = true;
+  # Don't strip the binary - it breaks bun compiled executables
+  dontStrip = true;
+
+  buildPhase = ''
+    runHook preBuild
+
+    cp -r ${node_modules}/node_modules .
+    chmod -R +w node_modules
+    export HOME=$TMPDIR
+    bun run build
+
+    runHook postBuild
+  '';
 
   installPhase = ''
     runHook preInstall
-    install -Dm755 $src $out/bin/bdui
+    install -Dm755 bdui $out/bin/bdui
     runHook postInstall
   '';
 
