f37b8a154133ff5b7421592769ffdd78bba8f28f
sgiath <sgiath@sgiath.dev>
2026-01-17 13:22:12 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- packages/dnd5etools/default.nix
+++ packages/dnd5etools/default.nix
@@ -65,7 +65,7 @@ let
     let
       img = fetchurl {
         pname = "5etools-img-${v.name}";
-        inherit version;
+        version = "2.22.2";
         inherit (v) hash;
         url = "https://github.com/5etools-mirror-2/5etools-img/releases/download/v${version}/img-v${version}.${v.name}";
       };
