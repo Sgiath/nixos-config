60d743dbb6403be70388a489cf597b4caddf5db6
sgiath <sgiath@sgiath.dev>
2026-07-28 10:45:14 +0000

    changes
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/agents/default.nix
+++ modules/home/sgiath/agents/default.nix
@@ -37,6 +37,7 @@
       pkgs.llm-agents.qmd
       pkgs.llm-agents.grok
       pkgs.llm-agents.codegraph
+      pkgs.llm-agents.amp
 
       pkgs.${namespace}.plannotator
       pkgs.${namespace}.clawpatch
