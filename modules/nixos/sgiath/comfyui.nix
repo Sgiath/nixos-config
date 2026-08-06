f531227b45996954e42db6b57ca1ae39e32da223
sgiath <sgiath@sgiath.dev>
2026-08-06 10:37:14 +0000

    chore(comfyui): disable the manager
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/nixos/sgiath/comfyui.nix
+++ modules/nixos/sgiath/comfyui.nix
@@ -8,7 +8,6 @@
   config = lib.mkIf config.services.comfyui.enable {
     services.comfyui = {
       gpuSupport = "rocm";
-      enableManager = true;
       extraArgs = [
         "--disable-xformers"
         "--use-pytorch-cross-attention"
