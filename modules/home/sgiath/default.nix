a4c6b884fb14f06c422f0c705ded13d92ef2bf8f
sgiath <sgiath@sgiath.dev>
2026-08-25 11:41:27 +0000

    chore(home): update FoundryVTT version to 14.367
    Signed-off-by: sgiath <sgiath@sgiath.dev>

--- modules/home/sgiath/default.nix
+++ modules/home/sgiath/default.nix
@@ -84,7 +84,7 @@
               ;;
 
             --vesta)
-              nix-store --add-fixed sha256 ~/nix-root/FoundryVTT-Linux-14.364.zip
+              nix-store --add-fixed sha256 ~/nix-root/FoundryVTT-Linux-14.367.zip
               NIX_SSHOPTS="-o IdentityAgent=$SSH_AUTH_SOCK" nixos-rebuild switch --sudo --flake '.#vesta' --target-host 'vesta.local'
               ;;
 
